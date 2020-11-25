499079de2d1274ea14073ca8e37f6d90e88961c6
unknown <mose23@naver.com>
2020-11-25 23:04:56 +0900

    Add BootStrap FontAwesome

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,20 @@
 import React from "react"
 import "./App.css"
+import { Container } from "react-bootstrap"
+
+import Header from "./components/Header"
+import Footer from "./components/Footer"
 
 const App = () => {
   return (
     <>
-      <main>
-        <h2>Hello World</h2>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h2>Hello World</h2>
+        </Container>
       </main>
+      <Footer />
     </>
   )
 }
