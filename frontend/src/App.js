7b449e1f9f92fe45e4577695250422cffda86151
unknown <mose23@naver.com>
2020-11-25 22:47:30 +0900

    React Setting

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+import "./App.css"
+
+const App = () => {
+  return (
+    <>
+      <main>
+        <h2>Hello World</h2>
+      </main>
+    </>
+  )
+}
+
+export default App
