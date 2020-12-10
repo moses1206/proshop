e76e6a452b260b4fbdba8321f9132b1845048f06
unknown <mose23@naver.com>
2020-12-10 22:56:11 +0900

    Admin Screen

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -2,7 +2,11 @@ import {} from "../constants/productConstants"
 import {
   USER_DETAILS_FAIL,
   USER_DETAILS_REQUEST,
+  USER_DETAILS_RESET,
   USER_DETAILS_SUCCESS,
+  USER_LIST_FAIL,
+  USER_LIST_REQUEST,
+  USER_LIST_SUCCESS,
   USER_LOGIN_FAIL,
   USER_LOGIN_REQUEST,
   USER_LOGIN_SUCCESS,
@@ -53,6 +57,8 @@ export const userDetailsReducer = (state = { user: {} }, action) => {
       return { loading: false, user: action.payload }
     case USER_DETAILS_FAIL:
       return { loading: false, error: action.payload }
+    case USER_DETAILS_RESET:
+      return { user: {} }
     default:
       return state
   }
@@ -68,6 +74,20 @@ export const userUpdateProfileReducer = (state = {}, action) => {
       return { loading: false, error: action.payload }
     case USER_UPDATE_PROFILE_RESET:
       return {}
+    default:
+      return state
+  }
+}
+
+export const userListReducer = (state = { users: [] }, action) => {
+  switch (action.type) {
+    case USER_LIST_REQUEST:
+      return { loading: true }
+    case USER_LIST_SUCCESS:
+      return { loading: false, users: action.payload }
+    case USER_LIST_FAIL:
+      return { loading: false, error: action.payload }
+
     default:
       return state
   }
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -13,12 +13,14 @@ import {
   userRegisterReducer,
   userDetailsReducer,
   userUpdateProfileReducer,
+  userListReducer,
 } from "./reducers/userReducers"
 
 import {
   orderCreateReducer,
   orderDetailsReducer,
   orderPayReducer,
+  orderListMyReducer,
 } from "./reducers/orderReducers"
 
 const reducer = combineReducers({
@@ -29,9 +31,11 @@ const reducer = combineReducers({
   userRegister: userRegisterReducer,
   userDetails: userDetailsReducer,
   userUpdateProfile: userUpdateProfileReducer,
+  userList: userListReducer,
   orderCreate: orderCreateReducer,
   orderDetails: orderDetailsReducer,
   orderPay: orderPayReducer,
+  orderListMy: orderListMyReducer,
 })
 
 const cartItemsFromStorage = localStorage.getItem("cartItems")
